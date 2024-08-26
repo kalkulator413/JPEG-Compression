4f1b331c46c1b9e230b8a52690d2b27be735298a
kalkulator413 <kalkin413@gmail.com>
2024-08-25 17:11:37 -0700

    setup project structure

---
+++ Lib/include/calc/Calc.h
@@ -0,0 +1,10 @@
+#ifndef LIB_INCLUDE_CALC_CALC_H_
+#define LIB_INCLUDE_CALC_CALC_H_
+
+class Calc {
+public:
+    int add(int a,int b);
+    int sub(int a,int b);
+};
+
+#endif /* LIB_INCLUDE_CALC_CALC_H_ */
